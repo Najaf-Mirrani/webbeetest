2398c124ea75aac79ce6c25d7c1600241a49117d
Najaf Ali <najaf.ali@intechww.com>
2023-04-20 00:37:47 +0500

    attempted

--- nest_typeorm/src/database/migrations/1663877813247-CinemaSystem.ts
+++ nest_typeorm/src/database/migrations/1663877813247-CinemaSystem.ts
@@ -31,8 +31,98 @@ export class CinemaSystem1663877813247 implements MigrationInterface {
    * As a cinema owner I dont want to configure the seating for every show
    */
   public async up(queryRunner: QueryRunner): Promise<void> {
-    throw new Error('TODO: implement migration in task 4');
+    await queryRunner.query(
+      `CREATE TABLE movie (
+        id INT NOT NULL AUTOINCREMENT PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        description TEXT,
+        image_url VARCHAR(255)
+      )`,
+    );
+
+    await queryRunner.query(
+      `CREATE TABLE cinema (
+        id INT NOT NULL AUTOINCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        address TEXT
+      )`,
+    );
+
+    await queryRunner.query(
+      `CREATE TABLE show (
+        id INT NOT NULL AUTOINCREMENT PRIMARY KEY,
+        start_time DATETIME NOT NULL,
+        end_time DATETIME NOT NULL,
+        movie_id INT NOT NULL,
+        cinema_id INT NOT NULL,
+        FOREIGN KEY (movie_id) REFERENCES movie (id),
+        FOREIGN KEY (cinema_id) REFERENCES cinema (id)
+      )`,
+    );
+
+    await queryRunner.query(
+      `CREATE TABLE show_room (
+        id INT NOT NULL AUTOINCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        cinema_id INT NOT NULL,
+        FOREIGN KEY (cinema_id) REFERENCES cinema (id)
+      )`,
+    );
+
+    await queryRunner.query(
+      `CREATE TABLE pricing (
+        id INT NOT NULL AUTOINCREMENT PRIMARY KEY,
+        show_id INT NOT NULL,
+        seat_type VARCHAR(255) NOT NULL,
+        price DECIMAL(10,2) NOT NULL,
+        FOREIGN KEY (show_id) REFERENCES show (id)
+      )`,
+    );
+
+    await queryRunner.query(
+      `CREATE TABLE seat (
+        id INT NOT NULL AUTOINCREMENT PRIMARY KEY,
+        show_room_id INT NOT NULL,
+        seat_number INT NOT NULL,
+        seat_type VARCHAR(255) NOT NULL,
+        is_booked BOOLEAN NOT NULL DEFAULT FALSE,
+        FOREIGN KEY (show_room_id) REFERENCES show_room (id)
+      )`,
+    );
+
+    await queryRunner.query(
+      ` CREATE TABLE "booking" (
+                "id" SERIAL NOT NULL PRIMARY KEY,
+                "showId" integer NOT NULL,
+                "seatId" integer NOT NULL,
+                "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
+                FOREIGN KEY ("showId") REFERENCES "show" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+                FOREIGN KEY ("seatId") REFERENCES "seat" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+            )`,
+    );
+
+    // throw new Error('TODO: implement migration in task 4');
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            DROP TABLE "booking"
+        `);
+
+    await queryRunner.query(`
+            DROP TABLE "seat"
+        `);
+
+    await queryRunner.query(`
+            DROP TABLE "show"
+        `);
+
+    await queryRunner.query(`
+            DROP TABLE "movie"
+        `);
+
+    await queryRunner.query(`
+            DROP TABLE "cinema"
+        `);
+  }
 }
